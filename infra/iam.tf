c4ccff3bdeff700e9fb8e250251e1192d550d59f
DavidDanso <desiingerboy@icloud.com>
2026-04-04 01:32:43 +0000

    feat: add AWSClarityReadOnly IAM role and policy for cross-account resource access

--- infra/iam.tf
+++ infra/iam.tf
@@ -35,3 +35,63 @@ resource "aws_iam_role_policy_attachment" "lambda_cross_account_attach" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = aws_iam_policy.lambda_cross_account.arn
 }
+
+resource "aws_iam_role" "clarity_read_only" {
+  name = "AWSClarityReadOnly"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          AWS = aws_iam_role.lambda_exec.arn
+        }
+        Condition = {
+          StringEquals = {
+            "sts:ExternalId" = "aws-clarity-scan"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "clarity_read_only_policy" {
+  name = "AWSClarityReadOnlyPolicy"
+  role = aws_iam_role.clarity_read_only.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:DescribeInstances",
+          "ec2:DescribeVolumes",
+          "ec2:DescribeSnapshots",
+          "ec2:DescribeAddresses",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeVpcs",
+          "ec2:DescribeSubnets",
+          "s3:ListAllMyBuckets",
+          "s3:GetBucketAcl",
+          "s3:GetBucketPolicy",
+          "s3:GetBucketPolicyStatus",
+          "s3:GetBucketPublicAccessBlock",
+          "s3:GetEncryptionConfiguration",
+          "s3:GetBucketLocation",
+          "s3:ListBucket",
+          "rds:DescribeDBInstances",
+          "iam:ListRoles",
+          "iam:GetRolePolicy",
+          "iam:ListRolePolicies",
+          "iam:ListAttachedRolePolicies",
+          "sts:GetCallerIdentity"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
